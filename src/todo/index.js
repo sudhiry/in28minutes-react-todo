b9af9a87072b7fb9031df2451bd84cfb619a2f81
Sudhir <sudhiryelikar@gmail.com>
2019-01-09 01:28:15 +0530

    Simplifying form validations

--- src/todo/index.js
+++ src/todo/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import moment from 'moment';
 import './todo.css';
 import Service from '../services';
@@ -7,15 +8,12 @@ export default class Todo extends Component {
 
     constructor(props) {
         super(props);
-        this.saveTodo = this.saveTodo.bind(this);
-        this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
-        this.handleTargetDateChange = this.handleTargetDateChange.bind(this);
+        this.validate = this.validate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
         this.state = {
             id: props.match.params.id,
             description: '',
-            targetDate: '',
-            invalidDscription: false,
-            invalidTargetDate: false,
+            targetDate: ''
         }
     }
 
@@ -29,26 +27,24 @@ export default class Todo extends Component {
         }
     }
 
-    validateForm() {
-        let invalidDscription = !this.state.description || this.state.description.length < 5;
-        let invalidTargetDate = !moment(this.state.targetDate).isValid();
-        this.setState({
-            invalidDscription,
-            invalidTargetDate
-        })
-        return invalidDscription || invalidTargetDate;
-
-    }
-
-    saveTodo(e) {
-        e.stopPropagation();
-        if(this.validateForm()) {
-            return;
+    validate(values) {
+        let errors = {};
+        if(!values.description) {
+            errors.description = "Enter valid values";
+        } else if(values.description.length < 5) {
+            errors.description = "Enter atleast 5 characters in Description"
         }
+        if(!moment(values.targetDate).isValid()) {
+            errors.targetDate = "Enter valid Target Date"
+        }
+        return errors;
+    }
+    
+    onSubmit(values) {
         if (this.state.id === -1) {
             Service.createTodo('in28minutes', {
-                description: this.state.description,
-                targetDate: this.state.targetDate
+                description: values.description,
+                targetDate: values.targetDate
             }).then(response => {
                 console.log(response.data);
                 this.props.history.push('/todos');
@@ -56,27 +52,13 @@ export default class Todo extends Component {
         } else {
             Service.updateTodo('in28minutes', this.state.id, {
                 id: this.state.id,
-                description: this.state.description,
-                targetDate: this.state.targetDate
+                description: values.description,
+                targetDate: values.targetDate
             }).then(response => {
                 console.log(response.data);
                 this.props.history.push('/todos');
             });
         }
-    }
-
-    handleDescriptionChange(e) {
-        this.setState({
-            invalidDscription: e.target.value.length < 5,
-            description: e.target.value
-        });
-    }
-
-    handleTargetDateChange(e) {
-        this.setState({
-            invalidTargetDate: !moment(e.target.value).isValid(),
-            targetDate: moment(e.target.value).format('YYYY-MM-DD')
-        });
     }
 
     render() {
@@ -85,25 +67,28 @@ export default class Todo extends Component {
             <>
                 <h1>Todo</h1>
                 <div className="container">
-                    {(this.state.invalidTargetDate || this.state.invalidDscription) && 
-                        <div className="alert alert-warning">Enter valid values</div>}
-                    {this.state.invalidTargetDate && 
-                        <div className="alert alert-warning">Enter valid Target Date</div>}
-                    {this.state.invalidDscription && 
-                        <div className="alert alert-warning"> Enter atleast 5 characters in Description</div >}
-                    <form>
-                        <fieldset className="form-group">
-                            <label>Description</label>
-                            <input type="text" className={`form-control ${this.state.invalidDscription && 'invalid'}`}
-                                name="description" value={description} onChange={this.handleDescriptionChange} />
-                        </fieldset>
-                        <fieldset className="form-group">
-                            <label>Target Date</label>
-                            <input type="date" className={`form-control ${this.state.invalidTargetDate && 'invalid'}`} name="targetDate"
-                                value={targetDate} onChange={this.handleTargetDateChange} onBlur={this.handleTargetDateChange} />
-                        </fieldset>
-                        <button type="button" className="btn btn-success" onClick={this.saveTodo} >Save</button>
-                    </form >
+                    <Formik 
+                        initialValues={{ description, targetDate }}
+                        validateOnBlur={false}
+                        enableReinitialize={true}
+                        validate={this.validate}
+                        onSubmit={this.onSubmit}>
+                        {({ errors, isSubmitting }) => (
+                            <Form>
+                                <ErrorMessage className="alert alert-warning" component="div" name="description" />
+                                <ErrorMessage className="alert alert-warning" component="div" name="targetDate" />
+                                <fieldset className="form-group">
+                                    <label>Description</label>
+                                    <Field className={`form-control ${errors.description? 'invalid' : ''}`} type="text" name="description" />
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <label>Target Date</label>
+                                    <Field className={`form-control ${errors.targetDate? 'invalid' : ''}`} type="date" name="targetDate" />
+                                </fieldset>
+                                <button className="btn btn-success" type="submit" disabled={isSubmitting}>Save</button>
+                            </Form>
+                        )}
+                    </Formik>
                 </div >
             </>
         );
